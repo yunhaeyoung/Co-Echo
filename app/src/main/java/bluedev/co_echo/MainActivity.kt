686230be90a50b70a5da9291754838b9c7d97171
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-07-26 21:54:45 +0900

    Initial commit

---
+++ app/src/main/java/bluedev/co_echo/MainActivity.kt
@@ -0,0 +1,11 @@
+package bluedev.co_echo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
